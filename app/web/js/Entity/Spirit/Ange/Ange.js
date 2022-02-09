c1e660aac0878178f9d4fa1afd652d77eac874bb
TrueBabyChaise <tomaugier23@gmail.com>
2022-02-09 17:54:14 +0100

    Remove useless stuff

--- app/web/js/Entity/Spirit/Ange/Ange.js
+++
@@ -1,6 +0,0 @@
-class Ange extends Spririt {
-	
-	constructor(name, x, y, owner, AngeId) {
-		super(name, new Sprite(new Model("js/assets/Entity/Ange/ange.png"), ctx), x, y, owner)
-	}
-}
--- app/web/js/Entity/Spirit/Human/Human.js
+++
@@ -1,6 +0,0 @@
-class Human extends Spririt {
-	
-	constructor(name, x, y, owner, AngeId) {
-		super(name, new Sprite(new Model("js/assets/Entity/Human/human.png"), ctx), x, y, owner)
-	}
-}
